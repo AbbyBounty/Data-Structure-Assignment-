d3a1e4b806cbd990833c404a662fa9fd0a81c04d
abhilash <abhiwardha@gmail.com>
2020-08-13 09:22:27 +0530

    aug 12

---
+++ 12Aug/Que 3 Qick sort/Que 3 Qick sort/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Que 3 Qick sort
+//
+//  Created by ABHILASH  on 12/08/20.
+//  Copyright © 2020 ABHILASH . All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+void quickSoert(int arr[],int size)
+{
+
+    
+}
+void display(int arr[],int size)
+{
+    
+}
+int main(int argc, const char * argv[]) {
+
+    
+    int arr[7]={5,7,2,4,1,9,3};
+    
+    quickSoert(arr,7);
+    display(arr,7);
+
+    return 0;
+}
