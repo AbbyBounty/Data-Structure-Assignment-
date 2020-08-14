55eb91e6498738a322d33e3ca29583c91d7b501a
abhilash <abhiwardha@gmail.com>
2020-08-14 17:41:52 +0530

    rotate array to left

--- 14Aug/reverse string/reverse string/main.cpp
+++ 14Aug/reverse string/reverse string/main.cpp
@@ -10,7 +10,23 @@
 #include <stack>
 #include <string>
 using namespace std;
-
+void rotation(int arr[],int num,string str,int size)
+{
+    
+        if(str=="left")
+        {
+            int rem=num%size;
+            for(int i=rem;i<size;i++){
+                cout<<arr[i]<<" ";
+            }
+            for(int i=0;i<rem;i++){
+                cout<<arr[i]<<" ";
+            }
+            cout<<endl;
+        }
+    
+       
+}
 int main(int argc, const char * argv[]) {
 
 //    string str="abcd";
@@ -20,20 +36,26 @@ int main(int argc, const char * argv[]) {
 //    cout<<str<<endl;
     
     
-    string str[20]={"Java"," ","is"," ","to"," ","JavaScript"," ","what"," ","car"," ","is"," ","to"," ","Carpet"};
+//    string str[20]={"Java"," ","is"," ","to"," ","JavaScript"," ","what"," ","car"," ","is"," ","to"," ","Carpet"};
+//
+//    for(int i=0;i<20;i++)
+//    {
+//        cout<<str[i];
+//        reverse(str[i].begin(), str[i].end());
+//    }
+//
+//        cout<<endl;
+//
+//    for(int i=0;i<20;i++)
+//        cout<<str[i];
+//
+//
+//    cout<<endl;
     
-    for(int i=0;i<20;i++)
-    {
-        cout<<str[i];
-        reverse(str[i].begin(), str[i].end());
-    }
     
-        cout<<endl;
     
-    for(int i=0;i<20;i++)
-        cout<<str[i];
-
+    int arr[5]={1,2,3,4,5};
     
-    cout<<endl;
+    rotation(arr,3,"left",5);
     return 0;
 }
