b556d8c5244b051678b0103ada7a45a047ab98b0
abhilash <abhiwardha@gmail.com>
2020-08-14 09:26:14 +0530

    a

--- 12Aug/Que 3 Qick sort/Que 3 Qick sort/main.cpp
+++ 12Aug/Que 3 Qick sort/Que 3 Qick sort/main.cpp
@@ -10,38 +10,35 @@
 using namespace std;
 
 void quickSort(int a[],int left,int right)
-
 {
     if(left>=right)
         return;
+    
     int i=left;
     int j=right;
- 
 
     while(i<j)
     {
-        while(i <= right && a[i]<=a[left])
+        while(i<=right && a[i]<=a[left])
             i++;
         while(a[j]>a[left])
             j--;
         if(i<j)
             swap(a[i],a[j]);
     }
-    
-
     swap(a[left],a[j]);
+    
     quickSort(a,left, j-1);
     quickSort(a, j+1, right);
-
-
 }
+
 void display(int arr[],int size)
 {
     for(int i=0;i<size;i++)
         cout<<arr[i]<<" ";
 }
-int main(int argc, const char * argv[]) {
 
+int main(int argc, const char * argv[]) {
     
     int arr[7]={5,7,2,4,1,9,3};
     
