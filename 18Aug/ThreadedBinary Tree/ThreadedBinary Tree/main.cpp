ad5745733c4eaacd484c4e36c77aff7d834d5605
abhilash <abhiwardha@gmail.com>
2020-08-18 17:55:56 +0530

    threaded binary tree

--- 18Aug/ThreadedBinary Tree/ThreadedBinary Tree/main.cpp
+++ 18Aug/ThreadedBinary Tree/ThreadedBinary Tree/main.cpp
@@ -9,39 +9,34 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-  
-    Node *left;
-     Node *right;
-    int Ltag;
-    int data;
-    int Rtag;
-    Node()
+
+class ThreadedBinary
+{
+private:
+    ThreadedBinary *left;
+        ThreadedBinary *right;
+       int Ltag;
+       int data;
+       int Rtag;
+public:
+   
+    ThreadedBinary()
     {
         this->data=this->Ltag=this->Ltag=0;
         this->left=this->right=NULL;
     }
     
-    Node()
+    ThreadedBinary(int data)
        {
-           this->data=datal
+           this->data=data;
            this->Ltag=this->Ltag=0;
            this->left=this->right=NULL;
        }
-    
-};
-
-
-class ThreadedBinary
-{
-private:
-    Node *root;
-public:
-    Node* inorderSuccessor(Node *p)
+    ThreadedBinary* inorderSuccessor(ThreadedBinary *p)
     {
-        Node *trav;
+        ThreadedBinary *trav;
         if(p->Rtag==0)
-            return p=>right;
+            return p->right;
         else{
             trav=p->right;
             while(trav->Ltag==1)
@@ -52,12 +47,65 @@ public:
     
     void inorder(ThreadedBinary *root)
     {
-        Node *p=inorderSuccessor(root);
+        cout<<"inorder"<<endl;
+        ThreadedBinary *p=inorderSuccessor(root);
         while(p!=root)
         {
             p=inorderSuccessor(p);
             cout<<p->data<<" ";
         }
+        cout<<endl;
+    }
+    
+    ThreadedBinary* preorderSuccessor(ThreadedBinary *p)
+    {
+        ThreadedBinary *trav;
+        if(p->Ltag==1)
+            return p->left;
+        else{
+            trav=p;
+            while(trav->Rtag==0)
+                trav=trav->right;
+            return trav->right;
+        }
+    }
+    
+    void preorder(ThreadedBinary *root)
+    {
+         cout<<"preorder"<<endl;
+        ThreadedBinary *p;
+        p=preorderSuccessor(root);
+        while(p!=root)
+        {
+            p=preorderSuccessor(p);
+            cout<<p-data<<" ";
+        }
+        cout<<endl;
+    }
+    
+    
+    
+    
+    
+    
+    
+    void add(ThreadedBinary *p,ThreadedBinary *q)
+    {
+        ThreadedBinary *temp;
+        q->right=p->right;
+        q->Rtag=p->Rtag;
+        q->left=p;
+        q->Ltag=0;
+        p->right=q;
+        p->Rtag=1;
+        if(q->Rtag==1)
+        {
+            temp=q->right;
+            while(temp->Ltag)
+                temp=temp->left;
+            temp->left=q;
+        }
+            
     }
 };
 int main(int argc, const char * argv[]) {
